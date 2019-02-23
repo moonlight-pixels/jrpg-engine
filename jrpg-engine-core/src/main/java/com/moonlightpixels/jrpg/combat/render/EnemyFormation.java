e399c6bc7cb48dc3d0d8e050d8d6a4c7485133dd
Jay St.Gelais <JayStGelais@gmail.com>
2019-02-22 20:42:22 -0500

    Battle Layout stuff

---
+++ jrpg-engine-core/src/main/java/com/moonlightpixels/jrpg/combat/render/EnemyFormation.java
@@ -0,0 +1,4 @@
+package com.moonlightpixels.jrpg.combat.render;
+
+public class EnemyFormation {
+}
--- jrpg-engine-core/src/main/java/com/moonlightpixels/jrpg/combat/render/internal/StandardCombatLayout.java
+++ jrpg-engine-core/src/main/java/com/moonlightpixels/jrpg/combat/render/internal/StandardCombatLayout.java
@@ -14,23 +14,46 @@ public final class StandardCombatLayout implements CombatLayout {
     @Getter
     private final float screenHeight;
     @Getter
+    private ScreenRegion uiRegion;
+    @Getter
     private ScreenRegion enemyRegion;
+    @Getter
     private ScreenRegion playerRegion;
 
     public StandardCombatLayout(final float screenWidth, final float screenHeight) {
         this.screenWidth = screenWidth;
         this.screenHeight = screenHeight;
+        resize(DEFAULT_UI_HEIGHT_PERCENT, DEFAULT_ENEMY_WIDTH_PERCENT);
+    }
+
+    public StandardCombatLayout setUiHeightPercent(final float uiHeightPercent) {
+        resize(uiHeightPercent, enemyRegion.getWidth() / screenWidth);
+        return this;
+    }
+
+    public StandardCombatLayout setenemyWidthPercent(final float enemyWidthPercent) {
+        resize(uiRegion.getHeight() / screenHeight, enemyWidthPercent);
+        return this;
+    }
+
+    private void resize(final float uiHeightPercent, final float enemyWidthPercent) {
+        uiRegion = ScreenRegion.builder()
+            .x(0f)
+            .y(0f)
+            .width(screenWidth)
+            .height(screenHeight * uiHeightPercent)
+            .build();
         enemyRegion = ScreenRegion.builder()
             .x(0f)
-            .y(screenHeight * DEFAULT_UI_HEIGHT_PERCENT)
-            .width(screenWidth * DEFAULT_ENEMY_WIDTH_PERCENT)
-            .height(screenHeight - screenHeight * DEFAULT_UI_HEIGHT_PERCENT)
+            .y(screenHeight * uiHeightPercent)
+            .width(screenWidth * uiHeightPercent)
+            .height(screenHeight - screenHeight * uiHeightPercent)
             .build();
         playerRegion = ScreenRegion.builder()
-            .x(screenWidth * DEFAULT_ENEMY_WIDTH_PERCENT)
-            .y(screenHeight * DEFAULT_UI_HEIGHT_PERCENT)
-            .width(screenWidth - screenWidth * DEFAULT_ENEMY_WIDTH_PERCENT)
-            .height(screenHeight - screenHeight * DEFAULT_UI_HEIGHT_PERCENT)
+            .x(screenWidth * enemyWidthPercent)
+            .y(screenHeight * uiHeightPercent)
+            .width(screenWidth - screenWidth * enemyWidthPercent)
+            .height(screenHeight - screenHeight * uiHeightPercent)
             .build();
     }
 
